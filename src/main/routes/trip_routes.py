8c87d8dc6ae66d1ec77c65b398ce1265875efffb
RanieryMendes <mendrc18@wfu.edu>
2024-07-09 13:16:08 -0400

    add most controllers

--- src/main/routes/trip_routes.py
+++ src/main/routes/trip_routes.py
@@ -1,8 +1,12 @@
 from flask import jsonify, Blueprint, request
 from src.controllers.trip_creator import TripCreator #import controller
+from src.controllers.trip_finder import TripFinder
+from src.controllers.link_creator import LinkCreator
+from src.controllers.trip_confirmation import TripConfirmation
 #import repos
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+from src.models.repositories.links_repository import LinksRepository
 #import db manager
 from src.models.settings.db_connection_handler import db_connection_handler
 
@@ -17,4 +21,35 @@ def create_trip():
     
     response = controller.create(request.json)
     
+    return jsonify(response["body"]), response["status_code"]
+
+@trip_routes_bp.route("/trips/<tripId>", methods=["GET"])
+def find_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    trips_repository =  TripsRepository(conn=conn)
+    controller = TripFinder(trips_repository=trips_repository)
+
+    response = controller.find_trip_details(tripId)
+
+    return jsonify(response["body"]), response["status_code"]
+
+
+@trip_routes_bp.route("/trips/<tripId>/confirm", methods=["GET"])
+def confirm_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    trips_repository =  TripsRepository(conn=conn)
+    controller = TripConfirmation(trips_repository=trips_repository)
+
+    response = controller.confirm(tripId)  
+
+    return jsonify(response["body"]), response["status_code"]
+
+@trip_routes_bp.route("/trips/<tripId>/confirm", methods=["POST"])
+def create_trip_link(tripId):
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn=conn)
+    controller = LinkCreator(link_repository=links_repository)
+
+    response = controller.create(request.json, trip_id=tripId)
+
     return jsonify(response["body"]), response["status_code"]
